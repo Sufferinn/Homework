7d68c98854547b9e04508348b73160cd1ae1fcce
Kim Yulian <xdepressionix@gmail.com>
2022-08-14 14:47:44 +0600

    Homework 2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Homework';
+  name = 'Yulian';
+  description = 'Studies at the 8th gymnasium in the 10th grade. Work experience: HTML, JS, Vue, Node, React.';
+  information1 = 'Ability to solve complex problems;';
+  information2 = 'Critical thinking;';
+  information3 = 'Creativity.'
+
+  cert1 = 'Foundation Certificate in English Literature';
+  cert2 = 'Foundation Certificate in History';
+  cert3 = 'Undergraduate Certificate in Archaeology';
+
+  image = 'https://png.pngtree.com/element_our/png/20181206/users-vector-icon-png_260862.jpg'
+
 }
